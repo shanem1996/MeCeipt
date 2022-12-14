ba1681d8f778ee84dbc9f47d3bec08f0e9196a28
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2022-12-14 21:54:13 +0000

    added onclick listener for sign up page

--- app/src/main/java/com/example/meceipt/WelcomeActivity.kt
+++ app/src/main/java/com/example/meceipt/WelcomeActivity.kt
@@ -1,7 +1,9 @@
 package com.example.meceipt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class WelcomeActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_welcome)
+
+       /* Setting the sign-up button in xml to a variable and adding a listener to it */
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+        btnSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+
+        }
     }
+
 }
