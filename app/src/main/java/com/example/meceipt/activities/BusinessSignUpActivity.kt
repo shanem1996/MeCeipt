1462ea35baa93e1629db4cd4c2fb46f56903eabb
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2022-12-16 18:47:43 +0000

    added retrofit dependencies

--- app/src/main/java/com/example/meceipt/activities/BusinessSignUpActivity.kt
+++ app/src/main/java/com/example/meceipt/activities/BusinessSignUpActivity.kt
@@ -2,11 +2,19 @@ package com.example.meceipt.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.meceipt.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 class BusinessSignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_business_sign_up)
+
     }
 }
