a5d51f1e0d61b257a43a00fdcb4052eb69fb43cc
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2023-04-25 18:06:54 +0100

    delete receipt

--- app/src/main/java/com/example/meceipt/adapter/UserReceiptAdapter.kt
+++ app/src/main/java/com/example/meceipt/adapter/UserReceiptAdapter.kt
@@ -1,12 +1,20 @@
 package com.example.meceipt.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meceipt.R
+import com.example.meceipt.activities.HomeActivity
+import com.example.meceipt.activities.ReceiptFragment
 import com.example.meceipt.databinding.ReceiptItemBinding
+import com.example.meceipt.databinding.UserReceiptItemBinding
 import com.example.meceipt.models.UserReceipt
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class UserReceiptAdapter(private val receiptList: List<UserReceipt>) : RecyclerView.Adapter<UserReceiptAdapter.UserReceiptViewHolder>() {
 
@@ -24,17 +32,28 @@ class UserReceiptAdapter(private val receiptList: List<UserReceipt>) : RecyclerV
     }
 
     inner class UserReceiptViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = ReceiptItemBinding.bind(itemView)
+        private val binding = UserReceiptItemBinding.bind(itemView)
         private val companyNameTv = binding.tvCompanyName
         private val dateTv = binding.tvDate
-        private val addBtn = binding.btnAdd
+        private val deleteBtn = binding.btnDelete
+
 
         fun bind(userReceipt: UserReceipt) {
             companyNameTv.text = userReceipt.companyName
             dateTv.text = userReceipt.date.toString()
 
-            addBtn.setOnClickListener{
+            val transactionNumber = userReceipt.transactionNumber.toString()
 
+            deleteBtn.setOnClickListener{
+                val user = FirebaseAuth.getInstance().currentUser?.uid.toString()
+                val documentRef = FirebaseFirestore.getInstance().collection("User").document(user).collection("Receipt").document(transactionNumber)
+
+                Toast.makeText(itemView.context, "Receipt Deleted!", Toast.LENGTH_SHORT)
+                    .show()
+
+                documentRef.delete()
+                val receiptFragment = Intent(itemView.context, HomeActivity::class.java)
+                itemView.context.startActivity(receiptFragment)
             }
         }
     }
