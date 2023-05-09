50bb0f81316c5e9dbf64843a024c281bfeedd305
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2023-05-09 18:25:17 +0100

    changed styling on sign in/sign up pages

--- app/src/main/java/com/example/meceipt/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/meceipt/adapter/UserAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.meceipt.adapter
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meceipt.R
+import com.example.meceipt.databinding.UserReceiptItemBinding
 import com.example.meceipt.models.User
 
 class UserAdapter(private val userList: List<User>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -25,6 +28,8 @@ class UserAdapter(private val userList: List<User>) : RecyclerView.Adapter<UserA
 
 
     inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = UserReceiptItemBinding.bind(itemView)
+        private val deleteBtn = binding.btnDelete
         private val userNameTv: TextView = itemView.findViewById(R.id.tvUserName)
         private val userEmailTv: TextView = itemView.findViewById(R.id.tvUserEmail)
 
@@ -32,6 +37,29 @@ class UserAdapter(private val userList: List<User>) : RecyclerView.Adapter<UserA
             userNameTv.text = user.fName
             userEmailTv.text = user.email
 
+            deleteBtn.setOnClickListener{
+                val deleteUserDialogBuilder = AlertDialog.Builder(itemView.context)
+                deleteUserDialogBuilder.setTitle("Delete User")
+                deleteUserDialogBuilder.setMessage("Are you sure you want to delete this user?")
+                deleteUserDialogBuilder.setPositiveButton("Delete User") { dialog, _ ->
+
+                }
+
+                deleteUserDialogBuilder.setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+
+                }
+
+                val deleteUserDialog = deleteUserDialogBuilder.create()
+                deleteUserDialog.setOnShowListener {
+                    deleteUserDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.setTextColor(
+                        ContextCompat.getColor(itemView.context, R.color.black))
+                    deleteUserDialog.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(
+                        ContextCompat.getColor(itemView.context, R.color.black))
+                }
+
+                deleteUserDialog.show()
+            }
 
         }
     }
